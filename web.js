7698059ed6596f44c0bb6e74715e501176269736
Ubuntu <ubuntu@ip-172-31-27-139.us-west-2.compute.internal>
2013-07-11 00:59:10 +0000

    modified index.html and changed web.js

--- web.js
+++ web.js
@@ -2,7 +2,9 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-//var buffer = fs.readFileSync('./index.html');
+var fs = require('fs');
+
+var buffer = fs.readFileSync('./index.html');
 
 var phrase = "Hello World 2!";
 
