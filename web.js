c6635c744ca28432529fd99273df11ccbf233a46
Ubuntu <ubuntu@ip-172-31-27-139.us-west-2.compute.internal>
2013-07-11 00:51:52 +0000

    modified index.html and changed web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 var buffer= fs.readFileSync('./index.html');
 
 app.get('/', function(request, response) {
-  response.send(buffer);
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
